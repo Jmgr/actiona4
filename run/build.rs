1d262cc3a36fbe92d380a82af632ceebd8f0745a
Jmgr <jmgr@jmgr.info>
2026-02-23 20:03:10 +0000

    Update deps

--- run/build.rs
+++ run/build.rs
@@ -35,12 +35,9 @@ fn main() {
     build_compressed_index_dts();
 
     #[cfg(windows)]
-    tauri_build::try_build(
-        tauri_build::Attributes::new().windows_attributes(
-            tauri_build::WindowsAttributes::new()
-                .app_manifest(include_str!("windows-manifest.xml")),
-        ),
-    )
+    tauri_build::try_build(tauri_build::Attributes::new().windows_attributes(
+        tauri_build::WindowsAttributes::new().app_manifest(include_str!("windows-manifest.xml")),
+    ))
     .expect("tauri build failed");
 
     #[cfg(not(windows))]
