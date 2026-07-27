03fd5a3a3e18823d1f8f3186af67e37af5e3a051
Jmgr <jmgr@jmgr.info>
2026-07-27 22:07:16 +0100

    feat: update Windows cert hash

--- crates/xtask/src/constants.rs
+++ crates/xtask/src/constants.rs
@@ -1,4 +1,4 @@
-pub const SIGNING_CERTIFICATE_SHA1: &str = "77DCCEE2A1B432AA2E361B49C620C85D994AB6D3";
+pub const SIGNING_CERTIFICATE_SHA1: &str = "C6EABAAB84DD1A7C841B95B86E937D7549879C6E";
 pub const SIGNING_TIMESTAMP_URL: &str = "http://timestamp.sectigo.com";
 pub const SIGNING_PRODUCT_URL: &str = "https://actiona.app";
 pub const RUN_FILE_DESCRIPTION: &str = "Actiona";
